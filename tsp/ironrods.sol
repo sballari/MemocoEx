5c61e0bca933f6b45289f74c915b588855611ddc
sballari <ballarinsimone@gmail.com>
2019-11-13 13:43:09 +0100

    f_obj OK (+- namesvar)

---
+++ tsp/ironrods.sol
@@ -0,0 +1,59 @@
+<?xml version = "1.0" encoding="UTF-8" standalone="yes"?>
+<CPLEXSolution version="1.2">
+ <header
+   problemName=""
+   solutionName="incumbent"
+   solutionIndex="-1"
+   objectiveValue="0"
+   solutionTypeValue="3"
+   solutionTypeString="primal"
+   solutionStatusValue="101"
+   solutionStatusString="integer optimal solution"
+   solutionMethodString="mip"
+   primalFeasible="1"
+   dualFeasible="1"
+   MIPNodes="0"
+   MIPIterations="0"
+   writeLevel="1"/>
+ <quality
+   epInt="1.0000000000000001e-05"
+   epRHS="9.9999999999999995e-07"
+   maxIntInfeas="0"
+   maxPrimalInfeas="0"
+   maxX="0"
+   maxSlack="0"/>
+ <variables>
+  <variable index="0" value="0"/>
+  <variable index="1" value="0"/>
+  <variable index="2" value="0"/>
+  <variable index="3" value="0"/>
+  <variable index="4" value="0"/>
+  <variable index="5" value="0"/>
+  <variable index="6" value="0"/>
+  <variable index="7" value="0"/>
+  <variable index="8" value="0"/>
+  <variable index="9" value="0"/>
+  <variable index="10" value="0"/>
+  <variable index="11" value="0"/>
+  <variable index="12" value="0"/>
+  <variable index="13" value="0"/>
+  <variable index="14" value="0"/>
+  <variable index="15" value="0"/>
+  <variable index="16" value="0"/>
+  <variable index="17" value="0"/>
+  <variable index="18" value="0"/>
+  <variable index="19" value="0"/>
+  <variable index="20" value="0"/>
+  <variable index="21" value="0"/>
+  <variable index="22" value="0"/>
+  <variable index="23" value="0"/>
+  <variable index="24" value="0"/>
+  <variable index="25" value="0"/>
+  <variable index="26" value="0"/>
+  <variable index="27" value="0"/>
+  <variable index="28" value="0"/>
+  <variable index="29" value="0"/>
+  <variable index="30" value="0"/>
+  <variable index="31" value="0"/>
+ </variables>
+</CPLEXSolution>
--- tsp/main.cpp
+++ tsp/main.cpp
@@ -4,16 +4,18 @@
 #include "../cpxmacro.h"
 
 //data initialization
-const int Nh = 3; //number of holes (nodes of the graph)
+const int Nh = 4; //number of holes (nodes of the graph)
 double Costs[Nh*Nh] = { //ridondante...
-    0.0,0.5,0.3,
-    0.5,0.0,0.3,
-    0.3,0.5,0.0
+//  0 1  2  3
+    0,10,15,30, //0
+    10,0,35,25, //1
+    15,35,0,30, //2
+    20,25,30,0  //3
 };
 int status;
 char errmsg[BUF_SIZE];
 
-const int startH = 0;
+const int startH = 0; //nodo partenza 
 
 
 int idX(int i, int j){
@@ -25,41 +27,42 @@ int idY(int i, int j){
 
 void setupLP(CEnv env, Prob lp){
 
-    //variables: x_i_j::Binary
-    std::vector<char> var_types(Nh*Nh);
-    std::vector<double> var_lbs(Nh*Nh);
-    std::vector<double> var_ubs(Nh*Nh);
-    std::vector<char*> var_names(Nh*Nh); 
-    std::vector<double> obj_fun_coefs(Nh*Nh);
+    
+    std::vector<char> var_types = std::vector<char>();
+    std::vector<double> var_lbs = std::vector<double>();
+    std::vector<double> var_ubs = std::vector<double>();
+    std::vector<char*> var_names = std::vector<char*>(); 
+    std::vector<double> obj_fun_coefs = std::vector<double>();       
 
     for(int i =0; i<Nh*Nh; i++){ //var x
         var_types.push_back('C');
         var_lbs.push_back(0.0);
         var_ubs.push_back(CPX_INFBOUND);
-        char* var_name;
+        char var_name[10] = "x_row_col";
         snprintf(var_name,10,"x_%c_%c",i/Nh,i%Nh);
         var_names.push_back(var_name);
         obj_fun_coefs.push_back(0.0);
     }
-
-    for(int i =0; i<Nh*Nh; i++){ 
+    
+    for(int i =0; i<Nh*Nh; i++){ //var y
         var_types.push_back('B');
         var_lbs.push_back(0.0);
         var_ubs.push_back(1.0);
-        char* var_name;
+        char var_name[10] = "y_row_col";
         snprintf(var_name,10,"y_%c_%c",i/Nh,i%Nh);
         var_names.push_back(var_name);
         obj_fun_coefs.push_back(Costs[i]);
     }
-    CHECKED_CPX_CALL( CPXnewcols, env, lp, 1, &obj_fun_coefs[0], &var_lbs[0], &var_ubs[0], &var_types[0], &var_names[0]);
+    CHECKED_CPX_CALL( CPXnewcols, env, lp, 2*Nh*Nh, &obj_fun_coefs[0], &var_lbs[0], &var_ubs[0], &var_types[0], nullptr);
     
     //CONSTRAINTS
+    
     std::vector<int> idx = std::vector<int>();
     std::vector<double> coef = std::vector<double>();
     std::vector<int> matbegin = std::vector<int>();
     std::vector<double> constValues = std::vector<double>();
     std::vector<char> senses = std::vector<char>();
-
+    /*
 
     int matbegin2use = 0;
 
@@ -70,12 +73,14 @@ void setupLP(CEnv env, Prob lp){
         if (k!=startH) {
             //somma archi entranti - somma archi uscenti da k
             for(int i=0; i<Nh; i++){ //archi entranti
-                idx.push_back(idX(i,k));
-		        coef.push_back(1);
-                matbegin2use++;
+                if (k!=i) {
+                    idx.push_back(idX(i,k));
+                    coef.push_back(1);
+                    matbegin2use++;
+                }
             }
             for(int j=0; j<Nh; j++){ //archi uscenti
-                if (j!=0) {
+                if (j!=0 || k!=j) {
                     idx.push_back(idX(k,j));
                     coef.push_back(-1);
                     matbegin2use++;
@@ -92,9 +97,11 @@ void setupLP(CEnv env, Prob lp){
         matbegin.push_back(matbegin2use);
 
         for (int j=0; j<Nh; j++){ //somma nodi uscenti
-            idx.push_back(idY(i,j));
-            coef.push_back(1);
-            matbegin2use++;
+            if (j!=i) {
+                idx.push_back(idY(i,j));
+                coef.push_back(1);
+                matbegin2use++;
+            }
         }
         senses.push_back('E');
         constValues.push_back(1);
@@ -104,9 +111,11 @@ void setupLP(CEnv env, Prob lp){
         matbegin.push_back(matbegin2use);
 
         for (int i=0; i<Nh; i++){//somma nodi entranti
-            idx.push_back(idY(i,j));
-            coef.push_back(1);
-            matbegin2use++;
+            if (j!=i){
+                idx.push_back(idY(i,j));
+                coef.push_back(1);
+                matbegin2use++;
+            }
         }
         senses.push_back('E');
         constValues.push_back(1);
@@ -120,15 +129,18 @@ void setupLP(CEnv env, Prob lp){
                 idx.push_back(idY(i,j));
                 coef.push_back(1);
                 coef.push_back(-Nh+1);
-                senses.push_back('E');
+                senses.push_back('L');
                 constValues.push_back(0);
                 matbegin2use+=2;
             }
         }
     }
-
-
-	CHECKED_CPX_CALL( CPXaddrows, env, lp, Nh*Nh*2, matbegin.size(), idx.size(), &constValues[0], &senses[0], &matbegin[0], &idx[0], &coef[0], nullptr, nullptr );
+    
+    //adding the A matrix
+    int constraints_N = constValues.size();
+    */
+    int constraints_N = 0;
+	CHECKED_CPX_CALL( CPXaddrows, env, lp, 0, constraints_N, idx.size(), &constValues[0], &senses[0], &matbegin[0], &idx[0], &coef[0], nullptr, nullptr );
 
 }   
 
@@ -140,6 +152,8 @@ int main(){
 		DECL_PROB( env, lp );
 		// setup LP
 		setupLP(env, lp);
+        //write the problem in a file
+        CHECKED_CPX_CALL( CPXwriteprob, env, lp, "lp_file/tsp.lp", NULL );
 		// optimize
 		CHECKED_CPX_CALL( CPXmipopt, env, lp );
 		// print
@@ -147,7 +161,7 @@ int main(){
 		CHECKED_CPX_CALL( CPXgetobjval, env, lp, &objval );
 		std::cout << "Objval: " << objval << std::endl;
 		int n = CPXgetnumcols(env, lp);
-		//if (n != 2*I*J+1) { throw std::runtime_error(std::string(__FILE__) + ":" + STRINGIZE(__LINE__) + ": " + "different number of variables"); }
+		
 	  	std::vector<double> varVals;
 		varVals.resize(n);
   		CHECKED_CPX_CALL( CPXgetx, env, lp, &varVals[0], 0, n - 1 );
